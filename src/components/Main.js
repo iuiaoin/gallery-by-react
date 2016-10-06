bda71ee1f0332d1695dbc081fff44c4e30aeb29d
LonelyObserver <478528180@qq.com>
2016-10-06 22:08:15 +0800

    init project structure

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+import React from 'react';
+
+//获取图片相关的数据
+let imageDatas = require('../data/imageDatas.json');
+
+//将图片名信息转成图片URL路径信息
+imageDatas = imageDatas.map(x => {
+  x.imgURL = require('../images/' + x.fileName);
+  return x;
+});
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section className="stage">
+        <section className="img-sec">
+
+        </section>
+        <nav className="controller-nav">
+
+        </nav>
+      </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
